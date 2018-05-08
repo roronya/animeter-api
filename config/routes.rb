b907a3021958706161ae746e8ed16f1984a4919d
roronya <roronya628@gmail.com>
2018-05-09 08:36:11 +0900

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :animes, only: [:index, :show]
+  resources :casts, only: [:index, :show]
+  resources :production_companies, only: [:index, :show]
+  resources :towatches
+  resources :users
+  resources :watcheds
+  resources :watchings
 end
